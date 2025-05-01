dfd637375c7b0f538cfaae12a30c22513ed1022d
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2025-05-02 00:00:18 +0530

    disabled flask

--- main.py
+++ main.py
@@ -218,4 +218,4 @@ if __name__ == '__main__':
     monitoring_thread.start()
     
     # Run the Flask app
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
